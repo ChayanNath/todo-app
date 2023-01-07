706a1a5386ae0c0785595d074463e1c822a20e61
chayanNath <chayannath19@gmail.com>
2023-01-08 02:13:52 +0530

    Storing user theme to local storage

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,19 @@ import LandingPage from "./components/LandingPage";
 import TodoProvider from "./context/todo/TodoProvider";
 
 function App() {
-  const [currentTheme, setCurrentTheme] = useState<string>("dark");
+  const colorTheme = localStorage.getItem("theme") || "dark";
+  const [currentTheme, setCurrentTheme] = useState<string>(colorTheme);
 
   const toggleThemeHandler = (): void => {
-    setCurrentTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
+    setCurrentTheme((prevTheme) => {
+      if (prevTheme === "dark") {
+        localStorage.setItem("theme", "light");
+        return "light";
+      } else {
+        localStorage.setItem("theme", "dark");
+        return "dark";
+      }
+    });
   };
   return (
     <TodoProvider>
